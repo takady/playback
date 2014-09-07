562100b1b060261281ccdc1c65d0d67e5063baa5
Yuichi Takada <takadyy@gmail.com>
2014-09-07 16:41:17 +0900

    add command line interface

---
+++ lib/playback/parser.rb
@@ -0,0 +1,54 @@
+module Playback
+  module Parser
+
+    def self.parse(line, format)
+      common_fields   = %w(request)
+      combined_fields = common_fields + %w(referer user_agent)
+
+      common_pattern     = '\S+\s+\S+\s+\S+\s+\[.*\]\s+"(\S+\s\S+\s\S+)"\s+\S+\s+\S+'
+      combined_pattern   = common_pattern + '\s+"([^"]*)"\s+"([^"]*)".*'
+
+      case format
+      when 'common'
+        fields = common_fields
+        pattern = /^#{common_pattern}$/
+      when 'combined'
+        fields = combined_fields
+        pattern = /^#{combined_pattern}$/
+      else
+        raise "format error\n no such format: <#{format}> \n"
+      end
+
+      matched = pattern.match(line)
+      raise "parse error\n at line: <#{line}> \n" if matched.nil?
+
+      generate_hash(fields, matched.to_a)
+    end
+
+    def self.generate_hash(keys, values)
+      hash = {}
+
+      keys.each.with_index do |key, idx|
+        key = key.to_sym
+        if (key == :request)
+          hash[key] = parse_request(values[idx+1])
+        else
+          hash[key] = values[idx+1]
+        end
+      end
+
+      hash
+    end
+
+    def self.parse_request(str)
+      method, path, protocol = str.split
+      {
+        method:   method,
+        path:     path,
+        protocol: protocol,
+      }
+    end
+
+    private_class_method :generate_hash, :parse_request
+  end
+end
--- lib/playback/request.rb
+++ lib/playback/request.rb
@@ -1,33 +1,52 @@
+require "playback/parser"
 require 'net/http'
-require 'apache_log/parser'
+require 'json'
 
 module Playback
   class Request
     DEFAULT_CONTENT_TYPE = 'application/text'
     DEFAULT_USER_AGENT = 'From Playback rubygems'
 
-    def initialize(uri, log, format)
-      @uri = uri
-      @log = log
-      @format = format
-      @parser = ApacheLog::Parser
+    def initialize(base_uri)
+      @base_uri = base_uri
+      @parser = Playback::Parser
     end
 
-    def run
-      responses = []
-      File.open @log do |file|
-        file.each_line do |line|
-          parsed_line = @parser.parse(line.chomp, @format)
-           response = request(parsed_line[:request][:method], parsed_line[:request][:path],
-                               parsed_line[:referer] ||= '', parsed_line[:user_agent] ||= DEFAULT_USER_AGENT)
-           responses.push response
+    def run(line, return_type='')
+      parsed_line = parse(line)
+      method = parsed_line[:request][:method]
+      path   = parsed_line[:request][:path]
+      referer = parsed_line[:referer] ||= ''
+      user_agent = parsed_line[:user_agent] ||= DEFAULT_USER_AGENT
+
+      res = request(method, path, referer, user_agent)
+
+      unless (return_type == 'net-http')
+        result = {
+          method: method,
+          path: path,
+          status: res.code.to_i,
+        }
+        res = JSON.generate result
+      end
+
+      res
+    end
+
+    def parse(line)
+      begin
+        @parser.parse(line.chomp, 'combined')
+      rescue Exception => e
+        begin
+          @parser.parse(line.chomp, 'common')
+        rescue Exception => e
+          puts 'error'
         end
       end
-      responses
     end
 
-    def request(method, path, referer='', user_agent=DEFAULT_USER_AGENT)
-      uri = URI.parse(@uri + path)
+    def request(method, path, referer, user_agent)
+      uri = URI.parse(@base_uri + path)
       http = Net::HTTP.new(uri.host, uri.port)
       query = uri.query.nil? ? '' : uri.query
       data = {'Content-Type' => DEFAULT_CONTENT_TYPE, 'Referer' => referer, 'User-Agent' => user_agent}
@@ -48,5 +67,6 @@ module Playback
       end
     end
 
+    private :parse, :request
   end
 end
