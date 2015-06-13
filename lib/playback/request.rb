eeab32f45e506494c4aede73016c7544417a5614
Yuichi Takada <takadyuichi@gmail.com>
2015-06-13 16:11:03 +0900

    cosme :lipstick:

--- lib/playback/request.rb
+++ lib/playback/request.rb
@@ -6,33 +6,27 @@ module Playback
   class Request
     DEFAULT_CONTENT_TYPE = 'application/text'
     DEFAULT_USER_AGENT = 'From Playback rubygems'
+    PARSER = ApacheLog::Parser
 
     def initialize(base_uri)
       @base_uri = base_uri
-      @common_parser = ApacheLog::Parser.new('common')
-      @combined_parser = ApacheLog::Parser.new('combined')
+      @common_parser = PARSER.new('common')
+      @combined_parser = PARSER.new('combined')
     end
 
     def run(line, return_type='')
       parsed_line = parse(line)
-      method = parsed_line[:request][:method]
-      path   = parsed_line[:request][:path]
-      referer = parsed_line[:referer] ||= ''
+
+      method     = parsed_line[:request][:method]
+      path       = parsed_line[:request][:path]
+      referer    = parsed_line[:referer] ||= ''
       user_agent = parsed_line[:user_agent] ||= DEFAULT_USER_AGENT
 
-      res = request(method, path, referer, user_agent)
+      response = request(method, path, referer, user_agent)
 
-      unless (return_type == 'net-http')
-        result = {
-          method: method,
-          path: path,
-          status: res.code.to_i,
-        }
-        res = JSON.generate(result)
-      end
+      return response if return_type == 'net-http'
 
-      res
-
+      {method: method, path: path, status: response.code.to_i}.to_json
     rescue => e
       e.message
     end
@@ -60,21 +54,21 @@ module Playback
 
       http = Net::HTTP.new(uri.host, uri.port)
       query = uri.query ||= ''
-      data = {'Content-Type' => DEFAULT_CONTENT_TYPE, 'Referer' => referer, 'User-Agent' => user_agent}
+      header = {'Content-Type' => DEFAULT_CONTENT_TYPE, 'Referer' => referer, 'User-Agent' => user_agent}
 
       case method
       when 'GET'
-        http.get(path, data)
+        http.get(path, header)
       when 'POST'
-        http.post(uri.path, query, data)
+        http.post(uri.path, query, header)
       when 'PUT'
-        http.put(uri.path, query, data)
+        http.put(uri.path, query, header)
       when 'DELETE'
-        http.delete(path, data)
+        http.delete(path, header)
       when 'PATCH'
-        http.patch(uri.path, query, data)
+        http.patch(uri.path, query, header)
       when 'HEAD'
-        http.head(path, data)
+        http.head(path, header)
       else
         raise "it is not supported http method: <#{method}>"
       end
